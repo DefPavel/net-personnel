378f68a36e59682c100cc15d549f0000f1e5c588
DefPavel <33838515+DefPavel@users.noreply.github.com>
2022-07-26 14:33:28 +0300

    async method

---
+++ Commands/Base/AsyncCommands.cs
@@ -0,0 +1,29 @@
+﻿namespace AlphaPersonel.Commands.Base;
+
+internal abstract class AsyncCommands : ICommand
+{
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    bool ICommand.CanExecute(object? parameter)
+    {
+        return CanExecute(parameter);
+    }
+    
+    void ICommand.Execute(object? parameter)
+    {
+        if (((ICommand)this).CanExecute(parameter))
+        {
+            ExecuteAsync(parameter);
+        }
+    }
+    protected virtual bool CanExecute(object p)
+    {
+        return true;
+    }
+    protected abstract Task ExecuteAsync(object p);
+
+}
